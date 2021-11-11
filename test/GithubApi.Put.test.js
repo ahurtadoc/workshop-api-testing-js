88af7b027aae79fdf05d3f565a342cf2997c549e
Alexander Hurtado Cardona <ahurtadoc@unal.edu.co>
2021-11-11 11:19:26 -0500

    PUT method consume

---
+++ test/GithubApi.Put.test.js
@@ -0,0 +1,39 @@
+const agent = require('superagent');
+const { StatusCodes } = require('http-status-codes');
+const chai = require('chai');
+
+const urlBase = 'https://api.github.com';
+const githubUserName = 'aperdomob';
+
+chai.use(require('chai-subset'));
+
+const { expect } = chai;
+
+describe('Github PUT method consume', () => {
+  it('Follow with authenticated user', async () => {
+    const response = await agent.put(`${urlBase}/user/following/${githubUserName}`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set({ 'User-Agent': 'ahurtadoc', 'Content-Length': 0 });
+    expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+    // eslint-disable-next-line no-unused-expressions
+    expect(response.body).to.be.empty;
+  });
+
+  it('Verify follow user', async () => {
+    const response = await agent.get(`${urlBase}/user/following`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set('User-Agent', 'ahurtadoc');
+    expect(response.status).to.equal(StatusCodes.OK);
+    const findedUser = response.body.find((user) => user.login === githubUserName);
+    expect(findedUser).to.not.equal(undefined);
+  });
+
+  it('Verify PUT idempotent method', async () => {
+    const response = await agent.put(`${urlBase}/user/following/${githubUserName}`)
+      .auth('token', process.env.ACCESS_TOKEN)
+      .set({ 'User-Agent': 'ahurtadoc', 'Content-Length': 0 });
+    expect(response.status).to.equal(StatusCodes.NO_CONTENT);
+    // eslint-disable-next-line no-unused-expressions
+    expect(response.body).to.be.empty;
+  });
+});
